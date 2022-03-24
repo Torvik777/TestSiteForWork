390e52282ef5f602be0684aeb1f317d786d29238
Torvik777 <79712569+Torvik777@users.noreply.github.com>
2022-03-24 02:44:03 +0200

    Main
    First and second block without adaptive

---
+++ script.js
@@ -0,0 +1,11 @@
+
+changeImg();
+function hideImg() 
+{
+    document.getElementsByClassName(img_acord_on).style.displey = "none";
+}
+function returnImg() 
+{
+    document.getElementsByClassName(img_acord_on).style.displey = "";
+}
+
